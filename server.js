be823d836d955514a5433a808c828b53a68feb53
Matthew Vaden <matthewvaden@gmail.com>
2020-08-10 18:10:55 -0400

    Logged messages for connecting to mongo.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to MongoDB Atlas."));
 
 db.once('open', () => console.log("Connected to MongoDB Atlas."));
 

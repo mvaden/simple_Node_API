73bc1ac3b90ec58e8e46c0732670673d9ae8af6c
Matthew Vaden <matthewvaden@gmail.com>
2020-08-10 18:16:58 -0400

    Middleware: Use JSON.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to MongoDB Atlas."));
 
-db.once('open', () => console.log("Connected to MongoDB Atlas."));
+app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server running on Port ${process.env.PORT}.`));
 

8d98338b005289289d036cd6257497e2e09b429b
Matthew Vaden <matthewvaden@gmail.com>
2020-08-10 18:24:19 -0400

    Scaffold the routes.

--- routes/players.js
+++ routes/players.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const router = express.Router();
+// Get all players
+router.get('/', (req, res) => {
+});
+
+// Get one player
+router.get('/:id', (req, res) => {
+});
+
+// Create one player
+router.post('/', (req, res) => {
+});
+
+// Update one player
+router.patch('/:id', (req, res) => {
+});
+
+// Delete one player
+router.delete('/:id', (req, res) => {
+});
+
 
 module.exports = router;
